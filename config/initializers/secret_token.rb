0b122d20db8b86ff653f86877576ba3a6b874896
Mihai <mihai@tldr.ro>
2014-02-20 22:26:30 +0200

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JsonRails::Application.config.secret_key_base = 'e820a2aaccc5f50a6015be37f62230915acb09cf1bd9250ce36e8af5d6748e720f7e00a4ba4b42f4f7124a52965beaff1ba5594a06c70a2b4ac1679793d692dc'
